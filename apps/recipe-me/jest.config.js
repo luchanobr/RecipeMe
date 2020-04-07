bcb09fb9cbd8526086fbfe360f7490725f5e9dab
luciano berardi rosmarin <luciano.rosmarin@gmail.com>
2020-04-07 17:07:17 -0300

    initial commit

---
+++ apps/recipe-me/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'recipe-me',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/recipe-me'
+};
