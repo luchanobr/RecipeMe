f9f5b04aefba99ad0a25d4e5d4d898216f5cfc90
luciano berardi rosmarin <luciano.rosmarin@gmail.com>
2020-04-08 10:34:18 -0300

    FEAT: api middlewares, mongoose paginate

--- apps/api/src/app/database/mongoDB.ts
+++ apps/api/src/app/database/mongoDB.ts
@@ -1,6 +1,8 @@
 import * as mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+import * as mongoosePaginate from "mongoose-paginate-v2"
+
 dotenv.config()
 
 mongoose.connect( process.env.MONGODB , {useNewUrlParser: true}).then(()=> {
@@ -9,4 +11,11 @@ mongoose.connect( process.env.MONGODB , {useNewUrlParser: true}).then(()=> {
     console.log(err)
 })
   
+mongoosePaginate.paginate.options = {
+    lean: true,
+    limit: 50
+}
+
+mongoose.plugin(mongoosePaginate) 
+
 export { mongoose }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,41 @@
 import * as express from 'express';
 import dotenv from 'dotenv'
 import {mongoose} from './app/database/mongoDB'
-
+import * as cors from 'cors'
+import * as helmet from 'helmet'
+import * as compression from 'compression'
+import * as lusca from 'lusca'
 dotenv.config()
 const app = express();
+//config
+const port = parseInt(process.env.port, 10) || 3333;
 
-
+// MongoDB
 const conection = mongoose.connection
 conection.on('error',  console.error.bind(console, 'connection error:'))
 conection.once('open', function() {
   console.log("conectado a mongoDB" )
 })
-const port = parseInt(process.env.port, 10) || 3333;
+
+// Middlewares
+app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: 'GET,PUT,POST,DELETE,OPTIONS,PACHT',
+  allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization'
+}))
+app.use(helmet())
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+
+
+//Routes
+
+
+
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
